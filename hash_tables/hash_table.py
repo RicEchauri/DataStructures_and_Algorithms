6a9be21705706cb1b28de9c88072f25eb72f7b57
Ricardo Corona <ricar0906@gmail.com>
2024-08-13 20:17:54 -0600

    Adding binarysearctrees and hash tables

---
+++ hash_tables/hash_table.py
@@ -0,0 +1,27 @@
+class HashTable:
+    def __init__(self, size = 7):
+        self.data_map = [None] * size
+
+    def __hash(self, key):
+        my_hash = 0
+        for letter in key:
+            my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
+        return my_hash  
+
+    def print_table(self):
+        for i, val in enumerate(self.data_map): 
+            print(i, ": ", val)
+            
+    def set_item(self, key, value):
+        index = self.__hash(key)
+        if not self.data_map[index]:
+            self.data_map[index] = []
+        self.data_map[index].append([key, value])
+        
+    def get_item(self, key):
+        index = self.__hash(key)
+        if self.data_map[index]:
+            for i in range(len(self.data_map[index])):
+                if self.data_map[index][i][0] == key:
+                    return self.data_map[index][i][1]
+        return None
